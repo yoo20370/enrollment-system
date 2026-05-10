bf5fa8d189becb4557f7a6a264109a0b50f5fa04
yoo20370 <yoo20370@gmail.com>
2026-05-10 22:05:41 +0900

    feat(common): ApiResponse 공통 응답 객체 추가

---
+++ src/main/java/com/github/yoo20370/enrollment/global/common/ApiResponse.java
@@ -0,0 +1,21 @@
+package com.github.yoo20370.enrollment.global.common;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ApiResponse<T> {
+    private final boolean success;
+    private final T data;
+
+    protected ApiResponse(boolean success, T data) {
+        this.success = success;
+        this.data = data;
+    }
+
+    public static <T> ApiResponse<T> of(T data) {
+        return new ApiResponse<>(true, data);
+    }
+
+}
