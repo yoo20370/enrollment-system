60254b4aa0f59ddfeef25258339dd10ac2e3b602
yoo20370 <yoo20370@gmail.com>
2026-05-10 19:32:56 +0900

    chore: init enrollment system

---
+++ src/main/java/com/github/yoo20370/enrollment/EnrollmentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.github.yoo20370.enrollment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnrollmentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnrollmentSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/yoo20370/enrollment/EnrollmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.yoo20370.enrollment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnrollmentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
