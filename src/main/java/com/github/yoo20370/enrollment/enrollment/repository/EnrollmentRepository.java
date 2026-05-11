9342a26e0cc4909356f6a3e6c561f77bd3195553
yoo20370 <yoo20370@gmail.com>
2026-05-12 02:55:08 +0900

    feat(enrollment): Enrollment 비관락을 위한 findWithLockById() 추가

--- src/main/java/com/github/yoo20370/enrollment/enrollment/repository/EnrollmentRepository.java
+++ src/main/java/com/github/yoo20370/enrollment/enrollment/repository/EnrollmentRepository.java
@@ -1,11 +1,16 @@
 package com.github.yoo20370.enrollment.enrollment.repository;
 
 import com.github.yoo20370.enrollment.enrollment.domain.Enrollment;
+import jakarta.persistence.LockModeType;
 import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 
 public interface EnrollmentRepository extends JpaRepository<Enrollment, UUID> {
 
     Optional<Enrollment> findByUserIdAndCourseId(UUID userId, UUID courseId);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    Optional<Enrollment> findWithLockById(UUID enrollmentId);
 }
