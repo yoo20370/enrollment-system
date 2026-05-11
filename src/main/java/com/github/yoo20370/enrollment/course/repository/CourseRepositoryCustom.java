231aa6596dbc9a9145b6a86c33d4dc67eec9972f
yoo20370 <yoo20370@gmail.com>
2026-05-11 15:55:39 +0900

    feat(course): 강의 목록 조회(상태 필터 적용) QueryDsl 코드 추가

--- src/main/java/com/github/yoo20370/enrollment/course/repository/CourseRepositoryCustom.java
+++ src/main/java/com/github/yoo20370/enrollment/course/repository/CourseRepositoryCustom.java
@@ -1,5 +1,11 @@
 package com.github.yoo20370.enrollment.course.repository;
 
+import com.github.yoo20370.enrollment.course.domain.Course;
+import com.github.yoo20370.enrollment.course.domain.CourseStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 public interface CourseRepositoryCustom {
 
+    Page<Course> findAllByStatus(CourseStatus status, Pageable pageable);
 }
--- src/main/java/com/github/yoo20370/enrollment/course/repository/CourseRepositoryCustomImpl.java
+++ src/main/java/com/github/yoo20370/enrollment/course/repository/CourseRepositoryCustomImpl.java
@@ -1,6 +1,17 @@
 package com.github.yoo20370.enrollment.course.repository;
 
+import static com.github.yoo20370.enrollment.course.domain.QCourse.course;
+
+import com.github.yoo20370.enrollment.course.domain.Course;
+import com.github.yoo20370.enrollment.course.domain.CourseStatus;
+import com.github.yoo20370.enrollment.course.domain.QCourse;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.support.PageableExecutionUtils;
 
 public class CourseRepositoryCustomImpl implements CourseRepositoryCustom{
 
@@ -10,4 +21,25 @@ public class CourseRepositoryCustomImpl implements CourseRepositoryCustom{
         this.queryFactory = queryFactory;
     }
 
+    @Override
+    public Page<Course> findAllByStatus(CourseStatus status, Pageable pageable) {
+
+        List<Course> content = queryFactory
+            .selectFrom(course)
+            .where(statusEq(status))
+            .offset(pageable.getOffset())
+            .limit(pageable.getPageSize())
+            .fetch();
+
+        JPAQuery<Long> countQuery = queryFactory
+            .select(course.count())
+            .from(course)
+            .where(statusEq(status));
+
+        return PageableExecutionUtils.getPage(content, pageable, countQuery::fetchOne);
+    }
+
+    private BooleanExpression statusEq(CourseStatus status) {
+        return status != null ? course.status.eq(status) : null;
+    }
 }
