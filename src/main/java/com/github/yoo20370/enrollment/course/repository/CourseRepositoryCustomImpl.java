9242d519297f253192a28c3179c269a44508bcbe
yoo20370 <yoo20370@gmail.com>
2026-05-12 21:08:05 +0900

    feat(course): 강의 상태 변경을 위한 조건부 업데이트 쿼리 구현

--- src/main/java/com/github/yoo20370/enrollment/course/repository/CourseRepositoryCustomImpl.java
+++ src/main/java/com/github/yoo20370/enrollment/course/repository/CourseRepositoryCustomImpl.java
@@ -4,11 +4,12 @@ import static com.github.yoo20370.enrollment.course.domain.QCourse.course;
 
 import com.github.yoo20370.enrollment.course.domain.Course;
 import com.github.yoo20370.enrollment.course.domain.CourseStatus;
-import com.github.yoo20370.enrollment.course.domain.QCourse;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
 import java.util.List;
+import java.util.UUID;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.support.PageableExecutionUtils;
@@ -16,9 +17,11 @@ import org.springframework.data.support.PageableExecutionUtils;
 public class CourseRepositoryCustomImpl implements CourseRepositoryCustom{
 
     private final JPAQueryFactory queryFactory;
+    private final EntityManager em;
 
-    public CourseRepositoryCustomImpl(JPAQueryFactory queryFactory) {
+    public CourseRepositoryCustomImpl(JPAQueryFactory queryFactory, EntityManager em) {
         this.queryFactory = queryFactory;
+        this.em = em;
     }
 
     @Override
@@ -37,6 +40,43 @@ public class CourseRepositoryCustomImpl implements CourseRepositoryCustom{
             .where(statusEq(status));
 
         return PageableExecutionUtils.getPage(content, pageable, countQuery::fetchOne);
+    }
+
+    @Override
+    public int open(UUID courseId, UUID userId) {
+
+        long result = queryFactory
+            .update(course)
+            .set(course.status, CourseStatus.OPEN)
+            .where(course.status.eq(CourseStatus.DRAFT)
+                .and(course.instructor.id.eq(userId)
+                    .and(course.id.eq(courseId))
+                )
+            )
+            .execute();
+
+        em.flush();
+        em.clear();
+
+        return (int) result;
+    }
+
+    @Override
+    public int close(UUID courseId, UUID userId) {
+        long result = queryFactory
+            .update(course)
+            .set(course.status, CourseStatus.CLOSED)
+            .where(course.status.eq(CourseStatus.OPEN)
+                .and(course.instructor.id.eq(userId)
+                    .and(course.id.eq(courseId))
+                )
+            )
+            .execute();
+
+        em.flush();
+        em.clear();
+
+        return (int) result;
     }
 
     private BooleanExpression statusEq(CourseStatus status) {
