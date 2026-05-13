4d9e8b0ce8d8c5ba6c915fdd95b6d6c461983818
yoo20370 <yoo20370@gmail.com>
2026-05-13 14:02:37 +0900

    fix(common): 예외 응답 ApiResponse로 감싸도록 수정 - 성공/실패 응답 일관성 있게 만들기 위함

--- src/main/java/com/github/yoo20370/enrollment/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/github/yoo20370/enrollment/global/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.github.yoo20370.enrollment.global.exception;
 
 
+import com.github.yoo20370.enrollment.global.common.ApiResponse;
 import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.ResponseEntity;
 
@@ -15,16 +16,16 @@ public class GlobalExceptionHandler {
 
     // 커스텀 예외 처리
     @ExceptionHandler(CustomException.class)
-    public ResponseEntity<ErrorResponse> handleCustomException(CustomException e) {
+    public ResponseEntity<ApiResponse<ErrorResponse>> handleCustomException(CustomException e) {
         ErrorCode errorCode = e.getErrorCode();
         return ResponseEntity
             .status(errorCode.getStatus())
-            .body(ErrorResponse.of(errorCode));
+            .body(ApiResponse.fail(ErrorResponse.of(errorCode)));
     }
 
     // @Valid 검증 실패 처리
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(
+    public ResponseEntity<ApiResponse<ErrorResponse>> handleMethodArgumentNotValidException(
         MethodArgumentNotValidException e) {
         String message = e.getBindingResult()
             .getFieldErrors()
@@ -33,38 +34,38 @@ public class GlobalExceptionHandler {
 
         return ResponseEntity
             .status(400)
-            .body(ErrorResponse.of(ErrorCode.COMMON_INVALID_INPUT, message));
+            .body(ApiResponse.fail(ErrorResponse.of(ErrorCode.COMMON_INVALID_INPUT, message)));
     }
 
     // @RequestParam, @PathVariable 검증 실패
     @ExceptionHandler(ConstraintViolationException.class)
-    public ResponseEntity<ErrorResponse> handleConstraintViolationException(
+    public ResponseEntity<ApiResponse<ErrorResponse>> handleConstraintViolationException(
         ConstraintViolationException e){
         return ResponseEntity
             .status(400)
-            .body(ErrorResponse.of(ErrorCode.COMMON_INVALID_INPUT, e.getMessage()));
+            .body(ApiResponse.fail(ErrorResponse.of(ErrorCode.COMMON_INVALID_INPUT, e.getMessage())));
     }
 
     // 헤드 누락 시 처리
     @ExceptionHandler(MissingRequestHeaderException.class)
-    public ResponseEntity<ErrorResponse> handleMissingRequestHeader(MissingRequestHeaderException e) {
+    public ResponseEntity<ApiResponse<ErrorResponse>> handleMissingRequestHeader(MissingRequestHeaderException e) {
         return ResponseEntity.status(400)
-            .body(ErrorResponse.of(ErrorCode.COMMON_INVALID_INPUT, e.getHeaderName() + " 헤더가 필요합니다."));
+            .body(ApiResponse.fail(ErrorResponse.of(ErrorCode.COMMON_INVALID_INPUT, e.getHeaderName() + " 헤더가 필요합니다.")));
     }
 
     // Enum에 해당하지 않는 값이 입력값으로 들어오는 경우 처리
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
-    public ResponseEntity<ErrorResponse> handleMethodArgumentTypeMismatch(MethodArgumentTypeMismatchException e) {
+    public ResponseEntity<ApiResponse<ErrorResponse>> handleMethodArgumentTypeMismatch(MethodArgumentTypeMismatchException e) {
         return ResponseEntity.status(400)
-            .body(ErrorResponse.of(ErrorCode.COMMON_INVALID_INPUT));
+            .body(ApiResponse.fail(ErrorResponse.of(ErrorCode.COMMON_INVALID_INPUT)));
     }
 
     // 그 외 예상치 못한 예외
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorResponse> handleException(Exception e) {
+    public ResponseEntity<ApiResponse<ErrorResponse>> handleException(Exception e) {
         return ResponseEntity
             .status(500)
-            .body(ErrorResponse.of(ErrorCode.INTERNAL_SERVER_ERROR));
+            .body(ApiResponse.fail(ErrorResponse.of(ErrorCode.INTERNAL_SERVER_ERROR)));
     }
 
 }
